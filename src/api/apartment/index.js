cb97df2bd8430237edc677baed86503a107ca9dc
Artur Stahov <vtemy.at.ua@gmail.com>
2021-06-06 23:48:28 +0300

    refactoring

--- src/api/apartment/index.js
+++ src/api/apartment/index.js
@@ -7,7 +7,7 @@ const guard = require('../../helpers/guard')
 
 router.get('/', guard, controllersApartment.getAll)
 router.get('/:id', guard, controllersApartment.getByID)
-router.get('/my', guard, controllersApartment.getAllMy)
+router.get('/my/', guard, controllersApartment.getAllMy)
 router.get('/my/:id', guard, controllersApartment.getByIDMy)
 router.post('/', guard, validateCreateApartment, controllersApartment.create)
 router.put('/my/:id', guard, validateUpdateApartment, controllersApartment.update)
